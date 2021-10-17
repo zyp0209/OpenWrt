0336693d6bffde6236ab59e63861bce447cd5197
hhaibo <85640068+hhaibo@users.noreply.github.com>
2021-10-17 17:43:08 +0800

    Update diy-part2.sh

--- mini/diy-part2.sh
+++ mini/diy-part2.sh
@@ -24,6 +24,7 @@ rm -rf package/lean/luci-app-jd-dailybonus
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
 git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
 svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman package/luci-app-dockerman
 
